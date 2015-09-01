983cd386fa525271130bd325768f2330858a1c63
duduvien <vien_pinkmoon@live.com>
2015-09-01 20:21:07 +0800

    Update mainPage.java

--- mainPage.java
+++ mainPage.java
@@ -27,3 +27,42 @@ public class mainPage extends JFrame{
        new mainPage(1);
    }
 }
+
+
+//********************************************************************
+//Example for GroupLayout
+//********************************************************************
+    //  glMainPage.createParallelGroup(GroupLayout.Alignment.LEADING)
+    //  glMainPage.addComponent()
+  /*.addComponent(c1)
+  .addComponent(c2)
+  .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+       .addComponent(c3)
+       .addComponent(c4))
+);
+layout.setVerticalGroup(
+layout.createSequentialGroup()
+  .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+       .addComponent(c1)
+       .addComponent(c2)
+       .addComponent(c3))
+  .addComponent(c4)
+);*/
+
+//********************************************************************
+    /*public class message extends JPanel {
+        public message(){
+          setLayout(new GridLayout(20,3,10,10));
+          JLabel lblHostName = new JLabel ("Host Name: ");
+          JLabel lblPortNumber = new JLabel ("Port Number: ");
+          JButton btnConnect = new JButton ("CONNECT");
+          add(lblPortNumber2);
+          add(lblHostName);
+          add(lblPortNumber);
+          add(btnConnect);
+
+        //  JOptionPane.showMessageDialog(null, "djdjdj", "InfoBox: Title", JOptionPane.INFORMATION_MESSAGE);
+          setVisible(true);
+        }
+    }*/
+//********************************************************************
