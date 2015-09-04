ecf3bac521f060cecd125cc2108e82c30d9281a8
vKongv <kaweng456@hotmail.com>
2015-09-04 15:48:17 +0800

    Concurrent server
    - Able to handle multiple client's request in the same time.

--- vkServer.java
+++ vkServer.java
@@ -1,31 +1,4 @@
-/** Version 3.0
-**/
-
-
-import java.net.*;
-import java.io.*;
-
-public class vkServer {
-    public static void main(String[] args) throws IOException {
-
-        int portNumber;
-        if (args.length != 1) {
-            System.out.println("Automatic assigned to port number 4444");
-            portNumber = 4444;
-
-        }
-        else{
-          portNumber = Integer.parseInt(args[0]);
-        }
-
-
-
-        try (
-            //Initialize the port number
-            ServerSocket serverSocket = new ServerSocket(portNumber);
-            Socket clientSocket = serverSocket.accept(); //Connection from client
-
-            PrintWriter out =
+PrintWriter out =
                 new PrintWriter(clientSocket.getOutputStream(), true); //Message that want to send to client
             BufferedReader in = new BufferedReader(
                 new InputStreamReader(clientSocket.getInputStream())); //Message that received from client
@@ -49,4 +22,3 @@ public class vkServer {
             System.out.println(e.getMessage());
         }
     }
-}
